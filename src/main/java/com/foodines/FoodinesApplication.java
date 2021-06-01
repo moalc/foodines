c2c5af9f1379d1a9b8a1bfbd550e31935c9fd1e7
Pedro Morillas Alcaide <pmoalc@gmail.com>
2021-06-01 20:48:31 +0200

    Initial commit

---
+++ src/main/java/com/foodines/FoodinesApplication.java
@@ -0,0 +1,13 @@
+package com.foodines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodines/FoodinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
